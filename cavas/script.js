82cf9b26ef59a95e482c41b3e652a610e4789177
Cai-Liming <cai.liming@iisgalvanimi.edu.it>
2022-01-05 10:47:45 +0000

    canvas

---
+++ cavas/script.js
@@ -0,0 +1,14 @@
+
+function startGame() {
+    myGameArea.start();
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 500;
+        this.canvas.height = 300;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
+}
