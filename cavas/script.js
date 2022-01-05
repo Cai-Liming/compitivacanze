f86de2a6485f66a3308b8d9ddfb3aaa65c350640
Cai-Liming <cai.liming@iisgalvanimi.edu.it>
2022-01-05 12:12:09 +0000

    quadratto

--- cavas/script.js
+++ cavas/script.js
@@ -1,14 +1,26 @@
-
-function startGame() {
+function startGame(){
     myGameArea.start();
+    myGameArea.draw(redSquare);
 }
 
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 500;
+        this.canvas.width =500;
         this.canvas.height = 300;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    },
+draw : function (component){
+    this.context.fillStyle = component.color; 
+    this.context.fillRect(component.x ,component.y , component.width ,component.height);
     }
 }
+
+var redSquare= {
+    width : 20,
+    height : 20,
+    x : 10,
+    y : 120,
+    color : "red"
+};
