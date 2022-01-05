8b98fd65a5cd729172b8deb2d6c04e09d2ad0ef0
Cai-Liming <cai.liming@iisgalvanimi.edu.it>
2022-01-05 12:45:33 +0000

    move

--- cavas/script.js
+++ cavas/script.js
@@ -1,6 +1,7 @@
 function startGame(){
     myGameArea.start();
     myGameArea.draw(redSquare);
+    
 }
 
 var myGameArea = {
@@ -10,10 +11,15 @@ var myGameArea = {
         this.canvas.height = 300;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval =setInterval(updateGameArea, 20);
     },
 draw : function (component){
     this.context.fillStyle = component.color; 
     this.context.fillRect(component.x ,component.y , component.width ,component.height);
+    },
+    move: function() {
+        redSquare.x = redSquare.x ;
+        redSquare.y = redSquare.y ;
     }
 }
 
@@ -24,3 +30,21 @@ var redSquare= {
     y : 120,
     color : "red"
 };
+
+function moveup() {
+    redSquare.y -=30;
+}
+function movedown() {
+    redSquare.y +=30;
+}
+function moveleft() {
+    redSquare.x -=30;
+}
+function moveright() {
+    redSquare.x +=30;
+}
+
+function updateGameArea(){
+    myGameArea.draw(redSquare);
+    
+}
