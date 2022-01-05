fc79da356712f920ee06bdafcc5320b076836c5a
Cai-Liming <cai.liming@iisgalvanimi.edu.it>
2022-01-05 15:52:02 +0000

    error drawImage

--- cavas/script.js
+++ cavas/script.js
@@ -1,6 +1,7 @@
 function startGame(){
     myGameArea.start();
     myGameArea.draw(redSquare);
+    animatedObject.loadImages();
     
 }
 
@@ -17,18 +18,66 @@ draw : function (component){
     this.context.fillStyle = component.color; 
     this.context.fillRect(component.x ,component.y , component.width ,component.height);
     },
+    clear : function(){
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    },
     move: function() {
-        redSquare.x = redSquare.x ;
-        redSquare.y = redSquare.y ;
-    }
+        redSquare.x = redSquare.x + redSquare.speedX;
+        redSquare.y = redSquare.y + redSquare.speedY;
+    },
+    drawGameObject: function(gameObject) {
+        this.context.drawImage(
+          gameObject.image,
+          gameObject.x,
+          gameObject.y,
+          gameObject.width,
+          gameObject.height
+        );
+      },
+
 }
 
+var animatedObject = {
+    speedX: 0,
+    speedY: 0,
+    width: 60,
+    height: 60,
+    x: 10,
+    y: 120,
+    imageList: [], //Vettore che conterrà tutte le immagini caricate
+    contaFrame: 0, //Tiene conto di quanti frame sono passati
+    actualFrame: 0, //Specifica quale frame disegnare
+  
+    update: function() {
+      this.x += this.speedX;
+      this.y += this.speedY;
+      this.contaFrame++;
+      if (this.contaFrame == 50) {
+        this.contaFrame = 0;
+        this.actualFrame = (1 + this.actualFrame) % this.imageList.length;
+        //console.log(this.actualFrame);
+        this.image = this.imageList[this.actualFrame];
+      }
+    },
+  
+    loadImages: function() {
+       for (imgPath of walking) {
+        var img = new Image(this.width, this.height);
+        img.src = imgPath;
+        this.imageList.push(img);
+        //console.log(img);
+      }
+      this.image = this.imageList[this.actualFrame];
+    }
+  };
 var redSquare= {
     width : 20,
     height : 20,
     x : 10,
     y : 120,
-    color : "red"
+    color : "red",
+    speedY : 0,
+    speedX : 0,
 };
 
 function moveup() {
@@ -44,7 +93,11 @@ function moveright() {
     redSquare.x +=30;
 }
 
+
 function updateGameArea(){
-    myGameArea.draw(redSquare);
-    
+    myGameArea.clear();
+    myGameArea.move();
+   myGameArea.draw(redSquare);
+    myGameArea.drawGameObject(animatedObject);
+    animatedObject.update();
 }
---
+++ cavas/sprtite.js
@@ -0,0 +1,12 @@
+var walking = [
+    "img.jpg/Walk (1).png",
+    "img.jpg/Walk (2).png",
+    "img.jpg/Walk (3).png",
+    "img.jpg/Walk (4).png",
+    "img.jpg/Walk (5).png",
+    "img.jpg/Walk (6).png",
+    "img.jpg/Walk (7).png",
+    "img.jpg/Walk (8).png",
+    "img.jpg/Walk (9).png",
+    "img.jpg/Walk (10).png"
+];
